d5eca145d02a685cd331668691418ea7f411a435
parthpatel133 <prpatel133@gmail.com>
2020-11-25 23:01:10 +0530

    conditional list and inline style problem solution

--- src/App.js
+++ src/App.js
@@ -1,85 +1,127 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Validation from './Validation/Validation'
+import Char from './Char/Char'
 import "./App.css";
-import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    persons: [
-      { id: "id1", name: "parth", age: 22 },
-      { id: "id2", name: "tushar", age: 22 },
-      { id: "id3", name: "viral", age: 21 },
-    ],
-    otherState: "some other value",
-    showPersons: true,
-  };
+    userInput: ''
+  }
 
-  nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex((p) => {
-      return p.id === id;
-    });
+  inputChangeHandler = (e) => {
+    this.setState({ userInput: e.target.value })
+  }
 
-    const person = { ...this.state.persons[personIndex] }; //copy object to immutate
-    // const person = Object.assign({}, this.state.persons[personIndex])   //other method
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({ persons: persons });
-  };
-
-  deletePersonHandler = (personIndex) => {
-    // const persons = thsq.state.persons.slice();  //to immutate the persons array
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  };
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({ userInput: updatedText });
+  }
 
   render() {
-    const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-    };
-
-    let persons = null;
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangeHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
-      );
-    }
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)}
+      />
+    })
 
     return (
-      <div className="App">
-        <h1>Hi, react App</h1>
-        <p>this is para inside h1</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Toggle Position
-        </button>
-        {persons}
+      <div>
+        <input type="text" onChange={this.inputChangeHandler} value={this.state.userInput} />
+        <p>The length of the text is {this.state.userInput}</p>
+        <Validation textLength={this.state.userInput.length} />
+        {charList}
       </div>
-    );
+    )
   }
 }
-
 export default App;
+
+// import React, { Component } from "react";
+// import "./App.css";
+// import Person from "./Person/Person";
+
+// class App extends Component {
+//   state = {
+//     persons: [
+//       { id: "id1", name: "parth", age: 22 },
+//       { id: "id2", name: "tushar", age: 22 },
+//       { id: "id3", name: "viral", age: 21 },
+//     ],
+//     otherState: "some other value",
+//     showPersons: true,
+//   };
+
+//   nameChangeHandler = (event, id) => {
+//     const personIndex = this.state.persons.findIndex((p) => {
+//       return p.id === id;
+//     });
+
+//     const person = { ...this.state.persons[personIndex] }; //copy object to immutate
+//     // const person = Object.assign({}, this.state.persons[personIndex])   //other method
+
+//     person.name = event.target.value;
+
+//     const persons = [...this.state.persons];
+//     persons[personIndex] = person;
+
+//     this.setState({ persons: persons });
+//   };
+
+//   deletePersonHandler = (personIndex) => {
+//     // const persons = this.state.persons.slice();  //to immutate the persons array
+//     const persons = [...this.state.persons];
+//     persons.splice(personIndex, 1);
+//     this.setState({ persons: persons });
+//   };
+
+//   togglePersonsHandler = () => {
+//     const doesShow = this.state.showPersons;
+//     this.setState({ showPersons: !doesShow });
+//   };
+
+//   render() {
+//     const style = {
+//       backgroundColor: "white",
+//       font: "inherit",
+//       border: "1px solid blue",
+//       padding: "8px",
+//       cursor: "pointer",
+//     };
+
+//     let persons = null;
+//     if (this.state.showPersons) {
+//       persons = (
+//         <div>
+//           {this.state.persons.map((person, index) => {
+//             return (
+//               <Person
+//                 click={() => this.deletePersonHandler(index)}
+//                 name={person.name}
+//                 age={person.age}
+//                 key={person.id}
+//                 changed={(event) => this.nameChangeHandler(event, person.id)}
+//               />
+//             );
+//           })}
+//         </div>
+//       );
+//     }
+
+//     return (
+//       <div className="App">
+//         <h1>Hi, react App</h1>
+//         <p>this is para inside h1</p>
+//         <button style={style} onClick={this.togglePersonsHandler}>
+//           Toggle Position
+//         </button>
+//         {persons}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
